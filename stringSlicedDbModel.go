1370f5447a85aa5982722d814798cd7605ff4503
a.zheltak <a.zheltak@rambler-co.ru>
2018-05-16 17:37:41 +0300

    more-5000

--- stringSlicedDbModel.go
+++ stringSlicedDbModel.go
@@ -4,6 +4,11 @@ import (
 	"sync"
 )
 
+const (
+	add    = iota
+	remove
+)
+
 type StringSlicedDbModel struct {
 	cache      map[string][]PotokStringSliceOrm
 	cacheMutex *sync.RWMutex
@@ -17,13 +22,14 @@ func (m *StringSlicedDbModel) init() {
 		m.cache = make(map[string][]PotokStringSliceOrm)
 		m.isInit = true
 		m.lenght = 0
-
 	}
 }
+
 func (m *StringSlicedDbModel) GetCache() interface{} {
 	m.init()
 	return m.cache
 }
+
 func (m *StringSlicedDbModel) FindInCache(id string) []PotokStringSliceOrm {
 	m.init()
 	m.cacheMutex.Lock()
@@ -44,20 +50,34 @@ func (m *StringSlicedDbModel) FindInCache(id string) []PotokStringSliceOrm {
 }
 
 func (m *StringSlicedDbModel) AddToCache(v PotokStringSliceOrm) {
+	m.cacheAction(v, add)
+}
+
+func (m *StringSlicedDbModel) RemoveFromCache(v PotokStringSliceOrm) {
+	m.cacheAction(v, remove)
+}
+
+func (m *StringSlicedDbModel) cacheAction(v PotokStringSliceOrm, action int) {
 	m.init()
 	var res []PotokStringSliceOrm
 	m.cacheMutex.Lock()
 	defer m.cacheMutex.Unlock()
 
 	current := m.cache[v.GetId()]
-	m.lenght = m.lenght - len(current)
+	m.lenght -= len(current)
 	for _, val := range current {
 		if val.GetRelationKey() != v.GetRelationKey() {
 			res = append(res, val)
 		}
 	}
-	m.lenght = len(res) + 1
-	m.cache[v.GetId()] = append(res, v)
+	m.lenght += len(res)
+
+	if action == add {
+		m.lenght += 1
+		res = append(res, v)
+	}
+
+	m.cache[v.GetId()] = res
 }
 
 func (m *StringSlicedDbModel) ClearCache() {
